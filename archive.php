486cf20915ec88b715e1bb85f7686bd2350873f4
Sk Md Morshed <skmorshed75@yahoo.com>
2020-01-27 16:25:26 +0600

    Class 18 : Wordpress Blog Archive (archive.php)

---
+++ archive.php
@@ -0,0 +1,51 @@
+<?php
+get_header();
+?>
+
+<div class="page-banner">
+	<div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/assets/images/ocean.jpg') ?>)"></div>
+	<div class="page-banner__content container container--narrow">
+	  <h1 class="page-banner__title">
+	  	<?php 
+	  	//Class 18
+	  	/*
+	  	if(is_category()){echo "Posts in "; single_cat_title(); } //Show Category title
+	  	if(is_author()){echo "Posted by "; the_author(); } //Show Author Name
+		OR
+		*/
+		the_archive_title();
+	  	?>
+
+	  </h1>
+	  <div class="page-banner__intro">
+	    <p>
+	    <?php
+	    the_archive_description();
+	    ?>
+	    </p>
+	  </div>
+	</div>  
+</div>
+
+<div class="container container--narrow page-section">
+	<?php
+	while(have_posts()) {
+		the_post(); ?>
+ 		<div class="post-item">
+ 			<h2 class = "headline headline--medium headline--post-title"><a href="<?php the_permalink()?>"><?php the_title(); ?></a></h2>
+ 		</div>
+
+ 		<div class="metabox">
+ 			<p>Posted By <b><?php the_author_posts_link(); ?></b> on <?php the_time('j.n.y'); ?> in <?php echo get_the_category_list(', ');?></p>
+ 		</div>
+
+ 		<div class="generic-content">
+ 			<?php the_excerpt(); ?>
+ 			<p><a class="btn btn--blue" href="<?php the_permalink(); ?>">Continue reading &raquo;</a></p>
+ 		</div>
+
+	<?php }
+	?>
+</div>
+
+<?php get_footer(); ?>
