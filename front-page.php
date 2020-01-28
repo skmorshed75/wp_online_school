37b98b61e8c3680d402d4059767d7137e069a3ee
Sk Md Morshed <skmorshed75@yahoo.com>
2020-01-28 15:24:53 +0600

    Class 21 : Wordpress Quick Edits & Improvements

--- front-page.php
+++ front-page.php
@@ -45,32 +45,30 @@ get_header();
     <div class="full-width-split__two">
       <div class="full-width-split__inner">
         <h2 class="headline headline--small-plus t-center">From Our Blogs</h2>
-<?php
-$homepagePosts = new WP_Query(array(
-  'posts_per_page' => 2,
-  //'post_type' => 'page'
+        <?php
+        $homepagePosts = new WP_Query(array(
+          'posts_per_page' => 2,
+          //'post_type' => 'page'
 
-));
-while($homepagePosts->have_posts()){
-  $homepagePosts->the_post();
-  ?>
-  <div class="event-summary">
-    <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
-      <span class="event-summary__month"><?php the_time('M'); ?></span>
-      <span class="event-summary__day"><?php the_time('d'); ?></span>  
-    </a>
-    <div class="event-summary__content">
-      <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-      <p><?php echo wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink(); ?>" class="nu gray"><?php echo esc_html("Read more", "test") ?></a></p>
-    </div>
-  </div>
-  <?php
-}
-wp_reset_postdata();
-?>
-        
-        
-        <p class="t-center no-margin"><a href="#" class="btn btn--yellow">View All Blog Posts</a></p>
+        ));
+        while($homepagePosts->have_posts()){
+          $homepagePosts->the_post();
+          ?>
+          <div class="event-summary">
+            <a class="event-summary__date event-summary__date--beige t-center" href="<?php the_permalink(); ?>">
+              <span class="event-summary__month"><?php the_time('M'); ?></span>
+              <span class="event-summary__day"><?php the_time('d'); ?></span>  
+            </a>
+            <div class="event-summary__content">
+              <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+              <p><?php echo wp_trim_words(get_the_content(), 18) ?><a href="<?php the_permalink(); ?>" class="nu gray"><?php echo esc_html("Read more", "test") ?></a></p>
+            </div>
+          </div>
+          <?php
+        }
+        wp_reset_postdata();
+        ?>
+        <p class="t-center no-margin"><a href="<?php echo site_url('/blog'); ?>" class="btn btn--yellow">View All Blog Posts</a></p>
       </div>
     </div>
   </div>
