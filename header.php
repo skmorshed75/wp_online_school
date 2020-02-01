7d0f058d899f27efcb749da0834e48d4fbad0821
Sk Md Morshed <skmorshed75@yahoo.com>
2020-02-01 19:49:00 +0600

    Class 31 : Past Events Page Custom Query Pagination

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
             </li>
             <li><a href="#">Programs</a></li>
             <li
-              <?php if(get_post_type() == 'event') echo 'class = "current-menu-item"'; ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
+              <?php if(get_post_type() == 'event' OR is_page('past-events')) echo 'class = "current-menu-item"'; ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
             <li><a href="#">Campuses</a></li>
             <li
               <?php if(get_post_type() == 'post') echo 'class = "current-menu-item"' ?>
