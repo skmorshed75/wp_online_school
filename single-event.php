2bf5808c8c090bc466e065a84e3d2c09545afa74
Sk Md Morshed <skmorshed75@yahoo.com>
2020-02-03 08:59:01 +0600

    Class 33 : Displaying WordPress Relationships Front End

--- single-event.php
+++ single-event.php
@@ -18,7 +18,22 @@ while(have_posts()){
 			<div class="metabox metabox--position-up metabox--with-home-link">
 		      <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('event'); ?>"><i class="fa fa-home" aria-hidden="true"></i><?php echo esc_html('&nbsp;Events Home','fictional'); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
 			</div>
-			<?php the_content(); ?>
+			<div class="generic-content"><?php the_content(); ?></div>
+			<!-- Class 33 -->
+			<?php
+			$relatedProgram = get_field('related_programs');
+			//print_r($relatedProgram);
+			if($relatedProgram){
+				echo '<hr class="section-break">';
+				echo '<h4 Class = "headline headline--medium">All Program(s)</h4>';
+				echo '<ul class = "link-list min-list">';
+				foreach($relatedProgram as $program){ ?>
+					<li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+				<?php }
+				echo '</ul>';
+			}
+
+			?>
 		</div>
 	</div>
 <?php	
--- single-program.php
+++ single-program.php
@@ -19,7 +19,63 @@ while(have_posts()){
 			<div class="metabox metabox--position-up metabox--with-home-link">
 		      <p><a class="metabox__blog-home-link" href="<?php echo get_post_type_archive_link('program'); ?>"><i class="fa fa-home" aria-hidden="true"></i><?php echo esc_html('&nbsp; All Programs','fictional'); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
 			</div>
-			<?php the_content(); ?>
+			<div class="generic-content"><?php the_content(); ?></div>
+
+				<?php
+		        // Class 33
+		        $today = date('Ymd');
+		        $homepageEvents = new WP_Query(array(
+		          'posts_per_page' => -1,
+		          'post_type' => 'event',
+		          'meta_key' => 'event_date',          
+		          'orderby' => 'meta_value_num',
+		          'order' => 'ASC',
+		          'meta_query' => array(
+		            array(
+		              'key' => 'event_date',
+		              'compare' => '>=',
+		              'value' => $today,
+		              'type' => 'numeric'
+		            ),
+		            array(
+		            	'key' => 'related_programs',
+		            	'compare' => 'LIKE',
+		            	'value' => '"'. get_the_ID() .'"'
+		            )
+		          )
+		        ));
+
+		        while($homepageEvents->have_posts()) {
+		          $homepageEvents->the_post(); ?>
+		          <div class="event-summary">
+		          <a class="event-summary__date t-center" href="#">
+		            <span class="event-summary__month"><?php 
+		              $eventDate = new DateTime(get_field('event_date'));
+		              echo $eventDate->format('M');
+		             ?></span>
+		            <span class="event-summary__day">
+		              <?php echo $eventDate->format('d'); ?>              
+		            </span>  
+		          </a>
+		          <div class="event-summary__content">
+		            <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+		            <p><?php 
+		              if(has_excerpt()){
+		                echo get_the_excerpt();
+		              } else {
+		                echo wp_trim_words(get_the_content(), 15);
+		              }
+		            ?> <a href="<?php the_permalink(); ?>" class="nu gray"><?php echo esc_html('Learn more','fictional') ?></a></p>
+		          </div>
+		        </div>
+
+		        
+		        
+		        <?php }
+
+
+		        ?>
+
 		</div>
 	</div>
 <?php	
